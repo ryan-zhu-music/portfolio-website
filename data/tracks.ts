7793d753667e85cbd18373c1f2b904627afb0cdc
RZ-Music <ryanzhu2005@gmail.com>
2022-11-29 12:02:19 -0500

    populate discography

--- data/tracks.ts
+++ data/tracks.ts
@@ -53,3 +53,417 @@ export const spotlight = [
     image: "/assets/img/Piano.png",
   },
 ];
+
+export const soundtracks = [
+  {
+    title: "Ancient Fantasies",
+    subtitle: "Evocative of fantasy sceneries and adventures.",
+    tracks: [
+      {
+        title: "To The Lands Beyond",
+        src: "/assets/audio/soundtracks/ToTheLandsBeyond.mp3",
+      },
+      {
+        title: "Wonderland",
+        src: "/assets/audio/soundtracks/Wonderland.mp3",
+      },
+      {
+        title: "Sail",
+        src: "/assets/audio/soundtracks/Sail.mp3",
+      },
+      {
+        title: "Atlantis",
+        src: "/assets/audio/soundtracks/Atlantis.mp3",
+      },
+      {
+        title: "Fallen",
+        src: "/assets/audio/soundtracks/Fallen.mp3",
+      },
+      {
+        title: "Ancient Awakening",
+        src: "/assets/audio/soundtracks/AncientAwakening.mp3",
+      },
+    ],
+    image: "/assets/img/AncientFantasies.png",
+  },
+  {
+    title: "Natura",
+    subtitle: "Encapsulating the wonder of mother nature.",
+    tracks: [
+      {
+        title: "Windward",
+        src: "/assets/audio/soundtracks/Windward.mp3",
+      },
+      {
+        title: "The Mist of the Fjords",
+        src: "/assets/audio/soundtracks/TheMistOfTheFjords.mp3",
+      },
+      {
+        title: "Amazon",
+        src: "/assets/audio/soundtracks/Amazon.mp3",
+      },
+      {
+        title: "Everest",
+        src: "/assets/audio/soundtracks/Everest.mp3",
+      },
+      {
+        title: "Arctic Apricity",
+        src: "/assets/audio/soundtracks/ArcticApricity.mp3",
+      },
+      {
+        title: "Azure Ascent",
+        src: "/assets/audio/soundtracks/AzureAscent.mp3",
+      },
+      {
+        title: "Arise!",
+        src: "/assets/audio/soundtracks/Arise.mp3",
+      },
+    ],
+    image: "/assets/img/Natura.png",
+  },
+  {
+    title: "Medieval Times",
+    subtitle: "Inspired by medieval scenes.",
+    tracks: [
+      {
+        title: "First Light",
+        src: "/assets/audio/soundtracks/FirstLight.mp3",
+      },
+      {
+        title: "Silvergreen",
+        src: "/assets/audio/soundtracks/Silvergreen.mp3",
+      },
+      {
+        title: "Rebirth",
+        src: "/assets/audio/soundtracks/Rebirth.mp3",
+      },
+      {
+        title: "Arcane Abyss",
+        src: "/assets/audio/soundtracks/ArcaneAbyss.mp3",
+      },
+    ],
+    image: "/assets/img/MedievalTimes.png",
+  },
+  {
+    title: "Little Fantasy Suite",
+    subtitle: "A programmitic suite inspired by VGM.",
+    tracks: [
+      {
+        title: "I - Prelude",
+        src: "/assets/audio/soundtracks/LittleFantasyPrelude.mp3",
+      },
+      {
+        title: "II - Waltz",
+        src: "/assets/audio/soundtracks/LittleFantasyWaltz.mp3",
+      },
+      {
+        title: "III - Finale",
+        src: "/assets/audio/soundtracks/LittleFantasyFinale.mp3",
+      },
+    ],
+    image: "/assets/img/LittleFantasySuite.png",
+  },
+];
+
+export const piano = [
+  {
+    title: "The Birthstones",
+    subtitle: "One for each of the twelve birthstones. Modern-style.",
+    tracks: [
+      {
+        title: "I - Garnet",
+        src: "/assets/audio/piano/1 - Garnet.mp3",
+      },
+      {
+        title: "II - Amethyst",
+        src: "/assets/audio/piano/2 - Amethyst.mp3",
+      },
+      {
+        title: "III - Aquamarine",
+        src: "/assets/audio/piano/3 - Aquamarine.mp3",
+      },
+      {
+        title: "IV - Diamond",
+        src: "/assets/audio/piano/4 - Diamond.mp3",
+      },
+      {
+        title: "V - Emerald",
+        src: "/assets/audio/piano/5 - Emerald.mp3",
+      },
+      {
+        title: "VI - Pearl",
+        src: "/assets/audio/piano/6 - Pearl.mp3",
+      },
+      {
+        title: "VII - Ruby",
+        src: "/assets/audio/piano/7 - Ruby.mp3",
+      },
+      {
+        title: "VIII - Peridot",
+        src: "/assets/audio/piano/8 - Peridot.mp3",
+      },
+      {
+        title: "IX - Sapphire",
+        src: "/assets/audio/piano/9 - Sapphire.mp3",
+      },
+      {
+        title: "X - Opal",
+        src: "/assets/audio/piano/10 - Opal.mp3",
+      },
+      {
+        title: "XI - Topaz",
+        src: "/assets/audio/piano/11 - Topaz.mp3",
+      },
+    ],
+    image: "/assets/img/TheBirthstones.png",
+  },
+  {
+    title: "Contemplations",
+    subtitle: "A set of reflective contemporary pieces.",
+    tracks: [
+      {
+        title: "I",
+        src: "/assets/audio/piano/Contemplation 1.mp3",
+      },
+      {
+        title: "II",
+        src: "/assets/audio/piano/Contemplation 2.mp3",
+      },
+      {
+        title: "III",
+        src: "/assets/audio/piano/Contemplation 3.mp3",
+      },
+      {
+        title: "IV",
+
+        src: "/assets/audio/piano/Contemplation 4.mp3",
+      },
+      {
+        title: "V",
+        src: "/assets/audio/piano/Contemplation 5.mp3",
+      },
+      {
+        title: "Sphinxes",
+        src: "/assets/audio/piano/Contemplation Sphinxes.mp3",
+      },
+    ],
+    image: "/assets/img/Contemplations.png",
+  },
+  {
+    title: "Piano Mix",
+    subtitle:
+      "A collection of compositions and arrangements from romantic to expressionist.",
+    tracks: [
+      {
+        title: "Halcyon Days",
+        src: "/assets/audio/piano/HalcyonDays.mp3",
+      },
+      {
+        title: "Ataraxia",
+        src: "/assets/audio/piano/Ataraxia.mp3",
+      },
+      {
+        title: "Endgame",
+        src: "/assets/audio/piano/Endgame.mp3",
+      },
+      {
+        title: "Pulsar",
+        src: "/assets/audio/piano/Pulsar.mp3",
+      },
+      {
+        title: "To The Stars",
+        src: "/assets/audio/piano/ToTheStars.mp3",
+      },
+      {
+        title: "Fugue on a Theme of Rachmaninoff",
+        src: "/assets/audio/piano/RachmaninoffFugue.mp3",
+      },
+      {
+        title: 'Atonal"Cereal" Fugue',
+        src: "/assets/audio/piano/AtonalFugue.mp3",
+      },
+      {
+        title: "Waltz in A Flat Major",
+        src: "/assets/audio/piano/WaltzInAFlatMajor.mp3",
+      },
+      {
+        title: "Chopin Etude Medley",
+        src: "/assets/audio/piano/ChopinEtudeMedley.mp3",
+      },
+      {
+        title: "Variations on a Theme of Beethoven",
+        src: "/assets/audio/piano/PathetiqueVariations.mp3",
+      },
+      {
+        title: "Coconut Rag",
+        src: "/assets/audio/piano/CoconutRag.mp3",
+      },
+      {
+        title: "Nocturne in F Minor",
+      },
+      {
+        title: "Polonaise in B Major",
+        src: "/assets/audio/piano/PolonaiseInBMajor.mp3",
+      },
+    ],
+    image: "/assets/img/PianoMix.png",
+  },
+];
+
+export const ensemble = [
+  {
+    title: "Concerto in C",
+    subtitle: "For piano and orchestra.",
+    tracks: [
+      {
+        title: "I - Allegro Moderato",
+        src: "/assets/audio/ensemble/Concerto 1.mp3",
+      },
+      {
+        title: "II - Andante",
+        src: "/assets/audio/ensemble/Concerto 2.mp3",
+      },
+      {
+        title: "III - Allegro",
+        src: "/assets/audio/ensemble/Concerto 3.mp3",
+      },
+    ],
+    image: "/assets/img/ConcertoInC.png",
+  },
+  {
+    title: "The High Cards",
+    subtitle: "A piano trio suite capturing the symbolism of each suit.",
+    tracks: [
+      {
+        title: "I - Ace of Spades",
+        src: "/assets/audio/ensemble/AceOfSpades.mp3",
+      },
+      {
+        title: "II - Ace of Hearts",
+        src: "/assets/audio/ensemble/AceOfHearts.mp3",
+      },
+      {
+        title: "III - Ace of Clubs",
+        src: "/assets/audio/ensemble/AceOfClubs.mp3",
+      },
+      {
+        title: "IV - Ace of Diamonds",
+        src: "/assets/audio/ensemble/AceOfDiamonds.mp3",
+      },
+    ],
+    image: "/assets/img/TheHighCards.png",
+  },
+  {
+    title: "Macbeth Suite",
+    subtitle: "A programmatic suite, one movement for each act.",
+    tracks: [
+      {
+        title: "I - Prophecies",
+        src: "/assets/audio/ensemble/Macbeth I.mp3",
+      },
+      {
+        title: "II - Murder",
+        src: "/assets/audio/ensemble/Macbeth II.mp3",
+      },
+      {
+        title: "III - Betrayal",
+        src: "/assets/audio/ensemble/Macbeth III.mp3",
+      },
+      {
+        title: "IV - Tyrant",
+        src: "/assets/audio/ensemble/Macbeth IV.mp3",
+      },
+      {
+        title: "V - Finale",
+        src: "/assets/audio/ensemble/Macbeth V.mp3",
+      },
+    ],
+    image: "/assets/img/Macbeth.png",
+  },
+  {
+    title: "Suite of the Elements",
+    subtitle: "An orchestral suite, inspired by the four elements of nature.",
+    tracks: [
+      {
+        title: "I - Water",
+        src: "/assets/audio/ensemble/1 - Water.mp3",
+      },
+      {
+        title: "II - Air",
+        src: "/assets/audio/ensemble/2 - Air.mp3",
+      },
+      {
+        title: "III - Earth",
+        src: "/assets/audio/ensemble/3 - Earth.mp3",
+      },
+      {
+        title: "IV - Fire",
+        src: "/assets/audio/ensemble/4 - Fire.mp3",
+      },
+    ],
+    image: "/assets/img/SuiteOfTheElements.png",
+  },
+  {
+    title: "Ensemble Mix",
+    subtitle:
+      "A collection of compositions and arrangements from romantic to modernist.",
+    tracks: [
+      {
+        title: "Evening Airs",
+        src: "/assets/audio/ensemble/EveningAirs.mp3",
+      },
+      {
+        title: "Fugue on a Theme of Rachmaninoff",
+        src: "/assets/audio/ensemble/RachmaninoffFugue.mp3",
+      },
+      {
+        title: "Fugue on a Christmas Folk Song",
+        src: "/assets/audio/ensemble/ChristmasFugue.mp3",
+      },
+      {
+        title: "Waltz in A Flat Major",
+        src: "/assets/audio/ensemble/Waltz No.1.mp3",
+      },
+      {
+        title: "Halcyon Days",
+        src: "/assets/audio/ensemble/HalcyonDays.mp3",
+      },
+      {
+        title: "Rachmaninoff - Prelude in D Minor",
+        src: "/assets/audio/ensemble/RachmaninoffPreludeInDMinor.mp3",
+      },
+      {
+        title: "Rachmaninoff - Prelude in C Minor",
+        src: "/assets/audio/ensemble/RachmaninoffPreludeInCMinor.mp3",
+      },
+      {
+        title: "C.G. - Fantasia Minore",
+        src: "/assets/audio/ensemble/FantasiaMinore.mp3",
+      },
+      {
+        title: "W.L. - Rhaposdy No. 1",
+        src: "/assets/audio/ensemble/Rhapsody1.mp3",
+      },
+      {
+        title: "D.H.L. - Waltz in A Flat Minor",
+      },
+      {
+        title: "D.D. - Prelude in A Flat Major",
+        src: "/assets/audio/ensemble/PreludeAFlatMajor.mp3",
+      },
+      {
+        title: 'Prelude - "Sacrifice"',
+        src: "/assets/audio/ensemble/SacrificePrelude.mp3",
+      },
+      {
+        title: "Waltz in C Sharp Minor",
+        src: "/assets/audio/ensemble/Waltz No.2.mp3",
+      },
+      {
+        title: "Taiko Phase",
+        src: "/assets/audio/ensemble/TaikoPhase.mp3",
+      },
+    ],
+    image: "/assets/img/EnsembleMix.png",
+  },
+];
