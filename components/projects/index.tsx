427f9219667d69e7d38248b7da5c608631bace71
RZ-Music <ryanzhu2005@gmail.com>
2022-11-30 13:27:13 -0500

    fix modals

--- components/projects/index.tsx
+++ components/projects/index.tsx
@@ -5,6 +5,7 @@ import Modal from "./modal";
 
 export default function Projects() {
   const [showModal, setShowModal] = useState(false);
+  const [selectedProject, setSelectedProject] = useState(null);
   return (
     <main className="relative w-full flex flex-col items-center justify-center py-32">
       <h1 className="text-white tracking-[0.5em] drop-shadow-xl font-elianto uppercase text-xl md:text-2xl lg:text-4xl pt-5">
@@ -14,12 +15,14 @@ export default function Projects() {
         className={`w-full h-44 my-10 overflow-x-scroll flex flex-row flex-nowrap`}
       >
         {projects.map((project) => (
-          <>
-            <Project project={project} setOpen={setShowModal} />
-            {showModal && <Modal project={project} setOpen={setShowModal} />}
-          </>
+          <Project
+            project={project}
+            setOpen={setShowModal}
+            setProject={setSelectedProject}
+          />
         ))}
       </div>
+      {showModal && <Modal project={selectedProject} setOpen={setShowModal} />}
     </main>
   );
 }
--- components/projects/modal.tsx
+++ components/projects/modal.tsx
@@ -2,11 +2,12 @@ import React from "react";
 
 export default function Modal({ project, setOpen }: any) {
   return (
-    <button
-      className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-50"
-      onClick={() => setOpen(false)}
-    >
-      <div className="w-5/6 h-5/6 gradient-mauve rounded-xl flex flex-row">
+    <div className="fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-50">
+      <button
+        className="absolute top-0 left-0 w-screen h-screen flex items-center justify-center bg-black bg-opacity-50 z-0"
+        onClick={() => setOpen(false)}
+      />
+      <div className="w-3/5 h-[70%] mt-16 gradient-mauve rounded-xl flex flex-row cursor-default z-10">
         <div className="w-2/5 h-full flex flex-col justify-center items-center">
           <img
             src={project.image}
@@ -14,37 +15,39 @@ export default function Modal({ project, setOpen }: any) {
             className="w-full h-full object-cover"
           />
         </div>
-        <div className="w-3/5 h-full flex flex-col justify-center items-center">
-          <h2 className="text-white text-2xl md:text-3xl lg:text-4xl font-elianto tracking-[0.3em] uppercase">
+        <div className="w-3/5 h-full flex flex-col justify-center items-start pr-14 pt-8 pb-4">
+          <h2 className="text-white text-xl md:text-xl lg:text-3xl font-elianto tracking-[0.3em] uppercase">
             {project.title}
           </h2>
-          <h3 className="font-montserrat italic font-bold text-sm text-mauve">
+          <h3 className="font-montserrat italic font-bold text-sm text-mauve my-2">
             October 2022 -
           </h3>
-          <p className="font-montserrat text-sm md:text-base lg:text-lg text-mauve font-normal">
-            {project.tech_stack.join("•")}
+          <p className="font-montserrat text-sm md:text-base lg:text-lg text-mauve font-normal my-2">
+            {project.tech_stack.join(" • ")}
           </p>
-          {project.description.map(
-            (paragraph: string, index: number) =>
-              paragraph && (
-                <p
-                  key={index}
-                  className="font-montserrat text-sm md:text-base lg:text-lg text-white font-normal"
-                >
-                  {paragraph}
-                </p>
-              )
-          )}
+          <div className="overflow-y-scroll">
+            {project.description.map(
+              (paragraph: string, index: number) =>
+                paragraph && (
+                  <p
+                    key={index}
+                    className="font-montserrat text-xs md:text-sm lg:text-base text-white font-normal text-start mb-3"
+                  >
+                    {paragraph}
+                  </p>
+                )
+            )}
+          </div>
           <a
             href={project.url}
             target="_blank"
             rel="noreferrer noopener"
-            className="text-white font-elianto uppercase tracking-[0.3em] hover:tracking-[0.4em] text-base md:text-lg gradient-mauve px-7 py-4 rounded-full font-semibold hover:shadow-sm hover:scale-105 duration-500 ease-in-out"
+            className="justify-self-end text-white font-elianto uppercase tracking-[0.3em] hover:tracking-[0.4em] text-xs md:text-sm ring-white ring-2 px-6 py-3 my-3 rounded-full font-semibold hover:shadow-sm duration-300 ease-in-out cursor-pointer"
           >
             {project.prototype ? "Prototype" : "Visit"}
           </a>
         </div>
       </div>
-    </button>
+    </div>
   );
 }
