9cffbb4b016dfd4a27a493d04c1bd554486bf631
RZ-Music <ryanzhu2005@gmail.com>
2022-12-03 13:25:43 -0500

    Head metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,98 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head></Head>
+      <Head>
+        <title>Ryan Zhu's Personal Site</title>
+        <meta
+          name="description"
+          content="Learn more about contemporary musician and full-stack developer Ryan Zhu, and browse through Ryan's collection of music compositions and projects."
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, 
+                   initial-scale=1.0"
+        />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="author" content="Ryan Zhu" />
+        <meta name="robots" content="index, nofollow" />
+        <meta name="copyright" content="2022 Ryan Zhu" />
+        <link rel="canonical" href="https://ryanzhu.com" />
+
+        <meta property="og:url" content="https://ryanzhu.com" />
+        <meta property="og:site_name" content="Ryan Zhu" />
+        <meta
+          property="og:title"
+          content="Contemporary musician and Full-Stack Developer"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="Learn more about contemporary musician and full-stack developer Ryan Zhu, and browse through Ryan's collection of music compositions and projects."
+        />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:image" content="/assets/img/og-image.png" />
+        <meta property="og:image:alt" content="Ryan Zhu" />
+        <meta property="og:image:width" content="512" />
+        <meta property="og:image:height" content="512" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@ryanzhumusic" />
+        <meta name="twitter:creator" content="@ryanzhumusic" />
+        <meta
+          name="twitter:title"
+          content="Contemporary musician and Full-Stack Developer"
+        />
+        <meta
+          name="twitter:description"
+          content="Learn more about contemporary musician and full-stack developer Ryan Zhu, and browse through Ryan's collection of music compositions and projects."
+        />
+        <meta
+          name="twitter:image"
+          content="https://ryanzhu.com/assets/icons/og.png"
+        />
+
+        <meta name="theme-color" content="#9989DB" />
+        <meta name="msapplication-navbutton-color" content="#9989DB" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#9989DB" />
+        <meta name="msapplication-TileColor" content="#9989DB" />
+        <link
+          rel="apple-touch-icon"
+          href="/assets/icons/apple-touch-icon-iphone-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/assets/icons/apple-touch-icon-ipad-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/assets/icons/apple-touch-icon-iphone-retina-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/assets/icons/apple-touch-icon-ipad-retina-152x152.png"
+        />
+        <link
+          rel="icon"
+          sizes="32x32"
+          href="/assets/icons/favicon-32x32.png"
+          type="image/png"
+        />
+        <link
+          rel="icon"
+          sizes="16x16"
+          href="/assets/icons/favicon-16x16.png"
+          type="image/png"
+        />
+        <link
+          rel="mask-icon"
+          href="/assets/icons/safari-pinned-tab.svg"
+          color="#6B6689"
+        />
+        <link rel="icon" href="/assets/icons/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Navbar from "../components/navbar";
 import Hero from "../components/hero";
 import About from "../components/about";
@@ -9,26 +8,14 @@ import Footer from "../components/footer";
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Ryan Zhu</title>
-        <meta name="description" content="Ryan Zhu's personal website" />
-        <meta
-          name="viewport"
-          content="width=device-width, 
-                   initial-scale=1.0"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="bg-[url('/assets/img/background.png')] bg-cover bg-top flex flex-col items-center justify-center">
-        <Navbar />
-        <Hero />
-        <About />
-        <Music />
-        <Projects />
-        <Contact />
-        <Footer />
-      </main>
-    </>
+    <main className="bg-[url('/assets/img/background.png')] bg-cover bg-top flex flex-col items-center justify-center">
+      <Navbar />
+      <Hero />
+      <About />
+      <Music />
+      <Projects />
+      <Contact />
+      <Footer />
+    </main>
   );
 }
